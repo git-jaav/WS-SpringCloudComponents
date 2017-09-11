87d9a370a90f24582b2fb47a5f77ef340be6d60d
Jose Arauco <araucovillar@gmail.com>
2017-09-11 01:08:53 -0500

    Add EurekaServer and fix Config

---
+++ JAEurekaServer/src/test/java/pe/jaav/sistemas/JAEurekaServer/JaEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.jaav.sistemas.JAEurekaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
